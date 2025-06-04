8dff4e38c944e57985e44b9b8bf37546a023c552
Franco Seiler <158525186+Fransei29@users.noreply.github.com>
2025-06-04 11:20:23 -0300

    Update Trip.js

--- src/components/Trip.js
+++ src/components/Trip.js
@@ -1,29 +1,46 @@
+'use client'
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlane, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+
 
 const Trip = () => {
   // Estado para almacenar el nombre del nuevo viaje y la lista de viajes
   const [name, setName] = useState('');
   const [trips, setTrips] = useState([]);
+  const [loading, setLoading] = useState(false);  // Estado para manejar el estado de carga
+  const [error, setError] = useState(''); // Estado para manejar los errores
 
   // Función para crear un nuevo viaje
   const createTrip = async () => {
     try {
-      await axios.post('http://localhost:3000/trip', { name }); // Enviar una solicitud POST al servidor para crear un nuevo viaje
+      setLoading(true);  // Iniciar el estado de carga
+      await axios.post('/api/trips', { name }); // Enviar una solicitud POST al servidor para crear un nuevo viaje
       fetchTrips(); // Volver a obtener la lista de viajes después de crear uno nuevo
-      setName(''); 
+      setName('');  // Limpiar el campo de entrada
+      setError('');  // Limpiar cualquier error anterior
     } catch (error) {
+      setError('Error creando el viaje: ' + (error.response?.data?.message || error.message)); // Mostrar el error
       console.error('Error creando el viaje:', error);
+    } finally {
+      setLoading(false);  // Finalizar el estado de carga
     }
   };
 
   // Función para obtener la lista de viajes
   const fetchTrips = async () => {
     try {
-      const response = await axios.get('http://localhost:3000/trips');  // Enviar una solicitud GET al servidor para obtener todos los viajes
+      setLoading(true);  // Iniciar el estado de carga
+      const response = await axios.get('/api/trips');  // Enviar una solicitud GET al servidor para obtener todos los viajes
       setTrips(response.data.trips);  // Actualizar el estado con la lista de viajes obtenida
+      setError('');  // Limpiar cualquier error anterior
     } catch (error) {
+      setError('Error obteniendo los viajes: ' + (error.response?.data?.message || error.message));  // Mostrar el error
       console.error('Error obteniendo los viajes:', error);
+    } finally {
+      setLoading(false);  // Finalizar el estado de carga
     }
   };
 
@@ -33,19 +50,33 @@ const Trip = () => {
   }, []);
 
   return (
-    <div className="box">
-      <h2>Trip</h2>
-      {/* Formulario para crear un nuevo viaje */}
-      <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-      <button onClick={createTrip}>Create New Trip</button>
-      {/* Lista de viajes */}
-      <ul>
-        {trips.map((trip) => (
-          <li key={trip._id}>{trip.name}</li>
-        ))}
-      </ul>
+    <div className="trip-container">
+      <div className="trip-inputs">
+        <h2><FontAwesomeIcon className="icons" icon={faPlane} /> Trips</h2>
+        <input 
+          type="text" 
+          value={name} 
+          onChange={(e) => setName(e.target.value)} 
+          placeholder="Enter trip name"
+        />
+        <button onClick={createTrip} disabled={loading}>
+          {loading ? 'Creating...' : 'Create New Trip'}
+        </button>
+
+        {error && <p style={{ color: 'red', marginTop: '0.5rem' }}>{error}</p>}
+      </div>
+
+      <div className="trip-list">
+        <h2 className='title-trips'><FontAwesomeIcon className="icons" icon={faMapMarkerAlt} /> Your Trips</h2>
+        <ul>
+          {trips?.map((trip) => (
+            <li key={trip._id}>{trip.name}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
 
 export default Trip;
+
