fb4ad4b01ec972cf5fffc6bb148a4fdb46bec301
Franco Seiler <158525186+Fransei29@users.noreply.github.com>
2025-06-04 11:19:34 -0300

    Update Title.js

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlane } from '@fortawesome/free-solid-svg-icons';
 
-const Title = ({ text, title }) => {
+const Title = () => {
     return (
-      <h1 className={title}>{text}</h1>
+    <div className='title'>
+
+
+      <h1 className='titlea'>
+        TripMate <FontAwesomeIcon className='iconA' icon={faPlane} />
+      </h1>
+      <p className='subtitle'>
+        Your ultimate companion for seamless travel planning and unforgettable journeys.
+      </p>
+
+    </div>
     );
   };
 
